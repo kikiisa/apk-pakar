94c55386581f5695a9d6cd7cc2117bf7d138bb4d
kikiisa <kikiisa89@gmail.com>
2022-08-09 12:41:41 +0800

    lupa icon

--- service-worker.js
+++ service-worker.js
@@ -4,14 +4,12 @@ const toCache = [
     'js/web.webmanifest',
     'css/about.1a49eb57.css',
     'css/app.676e8b36.css',
-    'css/chunk-vendors.bd0d8022.css',
     'js/about.6dd58e37.js',
     'js/about.6dd58e37.js.map',
     'js/app.08148684.js',
     'js/app.08148684.js.map',
-    'js/chunk-vendors.9ce14e71.js',
-    'js/chunk-vendors.9ce14e71.js.map',
     'js/register.js',
+    'img/favicon.png',
 ];
 
 self.addEventListener('install', function(event) {
