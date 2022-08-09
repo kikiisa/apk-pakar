d6adce1e39ed1ecf9330f4a01dd3bd3b856f5a73
kikiisa <kikiisa89@gmail.com>
2022-08-09 12:28:07 +0800

    version new

---
+++ service-worker.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = 'SW-005';
+const toCache = [
+    '/',
+    'css/about.1a49eb57.css',
+    'css/app.676e8b36.css',
+    'css/chunk-vendors.bd0d8022.css',
+    'js/web.webmanifest',
+    'js/about.6dd58e37.js',
+    'js/about.6dd58e37.js.map',
+    'js/app.08148684.js',
+    'js/app.08148684.js.map',
+    'js/chunk-vendors.9ce14e71.js',
+    'js/chunk-vendors.9ce14e71.js.map',
+    'js/register.js',
+    'img/priority.png',
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function(cache) {
+            return cache.addAll(toCache)
+        })
+        .then(self.skipWaiting())
+    )
+})
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        fetch(event.request)
+        .catch(() => {
+            return caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.match(event.request)
+            })
+        })
+    )
+})
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys()
+        .then((keyList) => {
+            return Promise.all(keyList.map((key) => {
+                if (key !== CACHE_NAME) {
+                    console.log('[ServiceWorker] Hapus cache lama', key)
+                    return caches.delete(key)
+                }
+            }))
+        })
+        .then(() => self.clients.claim())
+    )
+})
